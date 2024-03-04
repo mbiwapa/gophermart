943754fb064938c803273db1c7a0f51a4028024b
Vitaliy <mbiwapa72@gmail.com>
2024-03-04 11:26:41 +0300

    Проверка теории из пачки

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,78 +1,66 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
-	"github.com/jackc/pgx/v5/pgxpool"
-
-	"github.com/mbiwapa/gophermart.git/config"
-	"github.com/mbiwapa/gophermart.git/internal/app/http-server/server"
-	"github.com/mbiwapa/gophermart.git/internal/app/workers"
-	"github.com/mbiwapa/gophermart.git/internal/domain/entity"
 	"github.com/mbiwapa/gophermart.git/internal/lib/logger"
 )
 
 // run swag init -g internal/app/http-server/server/server.go to generate swagger docs
 // run swag fmt -g internal/app/http-server/server/server.go to format swagger docs
 func main() {
-	time.Sleep(3 * time.Second)
-	mainCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
+	//time.Sleep(3 * time.Second)
+	//mainCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	//defer stop()
 
 	log := logger.NewLogger()
 
 	log.Info("Loading configuration...")
-	conf := config.MustLoadConfig()
-
-	db, err := pgxpool.New(mainCtx, conf.DB)
-	if err != nil {
-		log.Error("Failed to connect to database", log.ErrorField(err))
-		os.Exit(1)
-	}
-	go func() {
-		<-mainCtx.Done()
-		log.Info("Closing database connection...")
-		db.Close()
-	}()
-
-	log.Info("Create order queue chanel...")
-	orderQueue := make(chan entity.Order, 100)
-	defer close(orderQueue)
-
-	log.Info("Create balance queue chanel...")
-	balanceQueue := make(chan entity.BalanceOperation, 100)
-	defer close(balanceQueue)
-
-	log.Info("Create error chanel ...")
-	errorChan := make(chan error)
-	defer close(errorChan)
-	go func() {
-		for orderErr := range errorChan {
-			log.Error("Error in order worker", log.ErrorField(orderErr))
-			os.Exit(1)
-		}
-		log.Info("Error chanel is closed")
-	}()
-
-	log.Info("Creating HTTP server...")
-	srv, err := server.New(mainCtx, conf, log, orderQueue, db)
-	if err != nil {
-		log.Error("Failed to create HTTP server", log.ErrorField(err))
-		os.Exit(1)
-	}
-	srv.Run()
-
-	orderWorker := workers.NewOrderWorker(mainCtx, log, orderQueue, errorChan, balanceQueue, db, conf.AccrualAdr)
-	orderWorker.Run()
-
-	balanceWorker := workers.NewBalanceWorker(mainCtx, log, balanceQueue, errorChan, db)
-	balanceWorker.Run()
-
-	<-mainCtx.Done()
-	time.Sleep(3 * time.Second)
+	//conf := config.MustLoadConfig()
+	//
+	//db, err := pgxpool.New(mainCtx, conf.DB)
+	//if err != nil {
+	//	log.Error("Failed to connect to database", log.ErrorField(err))
+	//	os.Exit(1)
+	//}
+	//go func() {
+	//	<-mainCtx.Done()
+	//	log.Info("Closing database connection...")
+	//	db.Close()
+	//}()
+	//
+	//log.Info("Create order queue chanel...")
+	//orderQueue := make(chan entity.Order, 100)
+	//defer close(orderQueue)
+	//
+	//log.Info("Create balance queue chanel...")
+	//balanceQueue := make(chan entity.BalanceOperation, 100)
+	//defer close(balanceQueue)
+	//
+	//log.Info("Create error chanel ...")
+	//errorChan := make(chan error)
+	//defer close(errorChan)
+	//go func() {
+	//	for orderErr := range errorChan {
+	//		log.Error("Error in order worker", log.ErrorField(orderErr))
+	//		os.Exit(1)
+	//	}
+	//	log.Info("Error chanel is closed")
+	//}()
+	//
+	//log.Info("Creating HTTP server...")
+	//srv, err := server.New(mainCtx, conf, log, orderQueue, db)
+	//if err != nil {
+	//	log.Error("Failed to create HTTP server", log.ErrorField(err))
+	//	os.Exit(1)
+	//}
+	//srv.Run()
+	//
+	//orderWorker := workers.NewOrderWorker(mainCtx, log, orderQueue, errorChan, balanceQueue, db, conf.AccrualAdr)
+	//orderWorker.Run()
+	//
+	//balanceWorker := workers.NewBalanceWorker(mainCtx, log, balanceQueue, errorChan, db)
+	//balanceWorker.Run()
+	//
+	//<-mainCtx.Done()
+	//time.Sleep(3 * time.Second)
 	log.Info("Good bye!")
 }
