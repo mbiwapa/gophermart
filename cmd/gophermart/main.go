104a26bbb95785b5f9c02c0e91524ccb474cc6a5
Vitaliy <mbiwapa72@gmail.com>
2024-03-04 12:21:39 +0300

    Проверка теории из пачки

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -12,6 +10,9 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/mbiwapa/gophermart.git/config"
+	"github.com/mbiwapa/gophermart.git/internal/app/http-server/server"
+	"github.com/mbiwapa/gophermart.git/internal/app/workers"
+	"github.com/mbiwapa/gophermart.git/internal/domain/entity"
 	"github.com/mbiwapa/gophermart.git/internal/lib/logger"
 )
 
@@ -38,57 +39,51 @@ func main() {
 		db.Close()
 	}()
 
-	fmt.Println("Hello, World!")
-	s := &http.Server{
-		Addr: ":8080",
-	}
+	//fmt.Println("Hello, World!")
+	//s := &http.Server{
+	//	Addr: ":8080",
+	//}
+	//go func() {
+	//	err := s.ListenAndServe()
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//}()
+	log.Info("Create order queue chanel...")
+	orderQueue := make(chan entity.Order, 100)
+	defer close(orderQueue)
+
+	log.Info("Create balance queue chanel...")
+	balanceQueue := make(chan entity.BalanceOperation, 100)
+	defer close(balanceQueue)
+
+	log.Info("Create error chanel ...")
+	errorChan := make(chan error)
+	defer close(errorChan)
 	go func() {
-		err := s.ListenAndServe()
-		if err != nil {
-			fmt.Println(err)
+		for orderErr := range errorChan {
+			log.Error("Error in order worker", log.ErrorField(orderErr))
+			os.Exit(1)
 		}
+		log.Info("Error chanel is closed")
 	}()
 
+	log.Info("Creating HTTP server...")
+	srv, err := server.New(mainCtx, conf, log, orderQueue, db)
+	if err != nil {
+		log.Error("Failed to create HTTP server", log.ErrorField(err))
+		os.Exit(1)
+	}
+	srv.Run()
+
+	orderWorker := workers.NewOrderWorker(mainCtx, log, orderQueue, errorChan, balanceQueue, db, conf.AccrualAdr)
+	orderWorker.Run()
+
+	balanceWorker := workers.NewBalanceWorker(mainCtx, log, balanceQueue, errorChan, db)
+	balanceWorker.Run()
+
 	<-mainCtx.Done()
-	_ = s.Shutdown(context.Background())
+	//_ = s.Shutdown(context.Background())
 	time.Sleep(3 * time.Second)
 	log.Info("Good bye!")
-
-	//
-	//log.Info("Create order queue chanel...")
-	//orderQueue := make(chan entity.Order, 100)
-	//defer close(orderQueue)
-	//
-	//log.Info("Create balance queue chanel...")
-	//balanceQueue := make(chan entity.BalanceOperation, 100)
-	//defer close(balanceQueue)
-	//
-	//log.Info("Create error chanel ...")
-	//errorChan := make(chan error)
-	//defer close(errorChan)
-	//go func() {
-	//	for orderErr := range errorChan {
-	//		log.Error("Error in order worker", log.ErrorField(orderErr))
-	//		os.Exit(1)
-	//	}
-	//	log.Info("Error chanel is closed")
-	//}()
-	//
-	//log.Info("Creating HTTP server...")
-	//srv, err := server.New(mainCtx, conf, log, orderQueue, db)
-	//if err != nil {
-	//	log.Error("Failed to create HTTP server", log.ErrorField(err))
-	//	os.Exit(1)
-	//}
-	//srv.Run()
-	//
-	//orderWorker := workers.NewOrderWorker(mainCtx, log, orderQueue, errorChan, balanceQueue, db, conf.AccrualAdr)
-	//orderWorker.Run()
-	//
-	//balanceWorker := workers.NewBalanceWorker(mainCtx, log, balanceQueue, errorChan, db)
-	//balanceWorker.Run()
-	//
-	//<-mainCtx.Done()
-	//time.Sleep(3 * time.Second)
-	//log.Info("Good bye!")
 }
